440de9ab1ba6b8d4d808e47a56ceba413afb0716
sidsrvstv <sidsrvstv@gmail.com>
2018-06-29 17:08:06 -0400

    hamming distance problem

---
+++ python/hamming/hamming.py
@@ -0,0 +1,13 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Hamming distance calculation only allowed between strands of equal lenght")
+
+    dist=0
+    for a,b in zip(strand_a, strand_b):
+        if a != b:
+            dist+=1
+
+    return dist
+
+
+print(distance("GGACG", "GGTCG"))
